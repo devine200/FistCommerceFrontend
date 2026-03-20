d978802fae7506a5b2f6ee15f6156f43c4a1a662
devine200 <samuelemeh266@gmail.com>
2026-03-20 16:01:15 +0100

    initial project dependencies installation

---
+++ tailwaind.config.ts
@@ -0,0 +1,15 @@
+export default {
+    content: ["./index.html", "./src/**/*.{ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#742f88",
+                secondary: "#ffffff",
+            },
+            fontFamily: {
+                sans: ['Outfit', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
